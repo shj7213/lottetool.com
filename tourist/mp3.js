8faffe07235f801676b9bbc2dd3f0423da839b84
shj7213 <65414610+shj7213@users.noreply.github.com>
2024-02-10 13:03:39 +0900

    Add files via upload

---
+++ tourist/mp3.js
@@ -0,0 +1,71 @@
+function a_1(){
+    var audio = document.getElementById("audio1_1");
+    audio.play();
+}
+function a_2(){
+    var audio = document.getElementById("audio1_2");
+    audio.play();
+}
+function a_3(){
+    var audio = document.getElementById("audio1_3");
+    audio.play();
+}
+function a_4(){
+    var audio = document.getElementById("audio1_4");
+    audio.play();
+}
+
+
+function b_1(){
+    var audio = document.getElementById("audio2_1");
+    audio.play();
+}
+function b_2(){
+    var audio = document.getElementById("audio2_2");
+    audio.play();
+}
+function b_3(){
+    var audio = document.getElementById("audio2_3");
+    audio.play();
+}
+function b_4(){
+    var audio = document.getElementById("audio2_4");
+    audio.play();
+}
+
+
+function c_1(){
+    var audio = document.getElementById("audio3_1");
+    audio.play();
+}
+function c_2(){
+    var audio = document.getElementById("audio3_2");
+    audio.play();
+}
+function c_3(){
+    var audio = document.getElementById("audio3_3");
+    audio.play();
+}
+function c_4(){
+    var audio = document.getElementById("audio3_4");
+    audio.play();
+}
+
+
+
+function d_1(){
+    var audio = document.getElementById("audio4_1");
+    audio.play();
+}
+function d_2(){
+    var audio = document.getElementById("audio4_2");
+    audio.play();
+}
+function d_3(){
+    var audio = document.getElementById("audio4_3");
+    audio.play();
+}
+function d_4(){
+    var audio = document.getElementById("audio4_4");
+    audio.play();
+}
